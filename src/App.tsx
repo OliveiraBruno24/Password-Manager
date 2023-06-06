947b28e1b6b8a168b8e79a5427ef7309c3ca0f36
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-06-06 09:58:39 -0300

    requisito 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
 
+import { Component } from 'react';
+import { Header } from './components/Header';
+
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
     </div>
   );
 }
