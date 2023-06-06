1f68e493bd8b7ea78b4edeb4c703d0b30101090e
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-06-06 10:27:01 -0300

    requisito 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 
 import { Component } from 'react';
 import { Header } from './components/Header';
+import { Form } from './components/Form';
 
 function App() {
   return (
     <div>
       <Header />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,42 @@
+export function Form() {
+  return (
+    <>
+      <label htmlFor="nome">
+        Nome do Serviço
+        <input
+          id="nome"
+          type="text"
+          name="Nome do Serviço"
+        />
+      </label>
+      <label htmlFor="login">
+        Login
+        <input
+          id="login"
+          type="text"
+          name="Login"
+          required
+        />
+      </label>
+      <label htmlFor="senha">
+        Senha
+        <input
+          id="senha"
+          type="password"
+          name="Senha"
+          required
+        />
+      </label>
+      <label htmlFor="URL">
+        URL
+        <input
+          id="URL"
+          type="text"
+          name="URL"
+        />
+      </label>
+      <button>Cadastrar</button>
+      <button>Cancelar</button>
+    </>
+  );
+}
